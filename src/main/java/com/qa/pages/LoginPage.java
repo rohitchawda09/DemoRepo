a521fe6f79a27fe0f6c955bdc631596c8bbea687
Rohit Chawda <A1FL0CEE@india.airtel.itm>
2021-10-30 17:18:13 +0530

    first git commit for my project

---
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -0,0 +1,8 @@
+package com.qa.pages;
+
+public class LoginPage {
+
+    public void login(){
+        System.out.println("LoginPage test");
+    }
+}
---
+++ src/test/java/com/qa/test/LoginTest.java
@@ -0,0 +1,8 @@
+package com.qa.test;
+
+public class LoginTest {
+
+    public void Test(){
+        System.out.println("Hello login test");
+    }
+}
